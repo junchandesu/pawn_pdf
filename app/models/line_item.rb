a452949b78576ee59b916aff152ec2849920ec9b
Junko Werner <aliciajace@gmail.com>
2016-01-26 18:04:25 -0800

    prepare data before PDF gem

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,3 +1,7 @@
 class LineItem < ActiveRecord::Base
   belongs_to :order
+
+  def total_price
+  	price * quantity
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ActiveRecord::Base
 	has_many :line_items
+
+	def order_total_price
+		line_items.to_a.sum{ |line_item| line_item.total_price}
+	end
 end
